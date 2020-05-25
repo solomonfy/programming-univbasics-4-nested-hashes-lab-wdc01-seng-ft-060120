117cc76c419aa9a98b5a1ba4a9c1cdccba21f8fe
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-05-25 22:27:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -138,7 +138,8 @@ def adding_to_dennis
       }
     }
   
-    programmer_hash[:dennis_ritchie][:languages] << ["Assembly"]
+    # programmer_hash[:dennis_ritchie][:languages] << ["Assembly"]
+    programmer_hash[:dennis_ritchie][:languages].push["Assembly"] 
     
     return programmer_hash
 
